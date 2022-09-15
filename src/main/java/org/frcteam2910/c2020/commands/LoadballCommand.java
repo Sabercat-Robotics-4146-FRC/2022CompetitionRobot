e199b01dfd6bf1a6c9aa7cdf7456b4e0e8bef552
AdamSmithey <asmithey66@susdgapps.org>
2022-09-15 15:58:30 -0700

    Added the LoadBallCommand and restructured indexer

---
+++ src/main/java/org/frcteam2910/c2020/commands/LoadballCommand.java
@@ -0,0 +1,17 @@
+package org.frcteam2910.c2020.commands;
+
+import org.frcteam2910.c2020.subsystems.Indexer;
+
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class LoadballCommand extends SequentialCommandGroup {
+
+    public LoadballCommand (Indexer indexer) {
+        addCommands(
+            new ToggleIndexerCommand(indexer, true), 
+            new WaitCommand(5.0), //TODO: Determine time
+            new ToggleIndexerCommand(indexer, false)
+        );
+    }
+}
