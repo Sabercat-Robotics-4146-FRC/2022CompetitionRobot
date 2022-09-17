45d1b3d72132bded8d0e4a80bb0c6f2dc19fec07
AdamSmithey <asmithey66@susdgapps.org>
2022-09-17 13:55:08 -0700

    Autonomous Changes

--- src/main/java/org/frcteam4146/c2022/autonomous/AutonomousFactory.java
+++ src/main/java/org/frcteam4146/c2022/autonomous/AutonomousFactory.java
@@ -3,57 +3,92 @@ package org.frcteam4146.c2022.autonomous;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import org.frcteam4146.c2022.RobotContainer;
+import org.frcteam4146.c2022.commands.drive.FollowTrajectoryCommand;
+import org.frcteam4146.c2022.subsystems.DrivetrainSubsystem;
+import org.frcteam4146.common.control.Path;
 import org.frcteam4146.common.control.Trajectory;
 import org.frcteam4146.common.control.TrajectoryConstraint;
 import org.frcteam4146.common.io.PathReader;
 
 public class AutonomousFactory {
+    RobotContainer container;
 
-  /*
-   * begins subsystems that will stay activated throughout the autonomous
-   */
-  public SequentialCommandGroup toggleSubsystems(RobotContainer container) {
-    SequentialCommandGroup command = new SequentialCommandGroup();
-
-    // add commands to toggle
-
-    return command;
-  }
-
-  /*
-   * creates an autonomous path.
-   */
-
-  public SequentialCommandGroup createPath(RobotContainer container, String pathDirectory) {
-    File root = new File(pathDirectory);
-    SequentialCommandGroup command = new SequentialCommandGroup();
-
-    File[] files = root.listFiles();
-
-    for (File f : files) {
-
-      if (f.isFile()) {
-        Trajectory trajectory;
-
-        try {
-          // create trajectory from JSON file
-          trajectory =
-              new Trajectory(
-                  new PathReader(new FileReader(f)).read(), // read JSON file and convert to Path
-                  new TrajectoryConstraint[0], // N/A (for the foreseeable future)
-                  10); // TODO find this value
-
-          // TODO: add commands
-
-        } catch (IOException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-      }
+    public AutonomousFactory(RobotContainer container) {
+        this.container = container;
     }
 
-    return command;
-  }
+    /*
+     * begins subsystems that will stay activated throughout the autonomous
+     */
+    public SequentialCommandGroup toggleSubsystems(RobotContainer container) {
+        SequentialCommandGroup command = new SequentialCommandGroup();
+
+        // add commands to toggle
+
+        return command;
+    }
+
+    /*
+     * creates an autonomous path.
+     */
+
+    public SequentialCommandGroup createPath(String pathDirectory) {
+        File root = new File(pathDirectory);
+        SequentialCommandGroup command = new SequentialCommandGroup();
+
+        File[] files = root.listFiles();
+
+        for (File f : files) {
+            if (f.isFile()) {
+
+                Trajectory trajectory = null;
+                
+                try {
+
+                    // create trajectory from JSON file
+                    PathReader pathReader = new PathReader(new FileReader(f));
+                    Path path = pathReader.read();
+
+                    trajectory = new Trajectory(
+                        path, // read JSON file and convert to Path
+                        new TrajectoryConstraint[0], // N/A
+                        0); // TODO find this value
+                    
+                    if (f.getName().toLowerCase().contains("ball")) {
+                        command.addCommands(createAutoBallCommand(trajectory, f));
+                    } else {
+                        command.addCommands(createAutoNonBallCommand(trajectory, f));
+                    }
+                } catch (Exception e) {}
+            }
+        }
+
+        return command;
+    }
+
+    /*
+     * creates auto command that interacts with a ball
+     */
+    public SequentialCommandGroup createAutoBallCommand(Trajectory trajectory, File f) throws Exception {
+        SequentialCommandGroup command = new SequentialCommandGroup();
+        
+        // command.addCommands(new FollowTrajectoryCommand(drivetrain, trajectory),
+                            // new LoadBallCommand()
+                            // new ShootBallCommand());
+        // TODO: add commands
+
+        return command;
+    }
+
+    /*
+     * creates auto command that does not interact with a ball
+     */
+    public SequentialCommandGroup createAutoNonBallCommand(Trajectory trajectory, File f) throws Exception {
+        SequentialCommandGroup command = new SequentialCommandGroup();
+        
+        command.addCommands(new FollowTrajectoryCommand(container.getDrivetrainSubsystem(), trajectory));
+
+        return command;
+    }
 }
--- src/main/java/org/frcteam4146/c2022/autonomous/AutonomousSelector.java
+++ src/main/java/org/frcteam4146/c2022/autonomous/AutonomousSelector.java
@@ -4,76 +4,65 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.frcteam4146.c2022.RobotContainer;
 
 public class AutonomousSelector {
-  private SendableChooser<String> pathChooser = new SendableChooser<>();
-  private RobotContainer container;
+    private SendableChooser<String> pathChooser = new SendableChooser<>();
+    private RobotContainer container;
 
-  public AutonomousSelector(RobotContainer container) {
-    this.container = container;
+    public AutonomousSelector(RobotContainer container) {
+        this.container = container;
 
-    File directory = Filesystem.getDeployDirectory();
+        File directory = Filesystem.getDeployDirectory();
 
-    // List of all autonomous paths folders
-    List<File> paths =
-        new ArrayList<File>() {
-          {
-            /*
-             * populate List
-             */
-            File pathsFile = null;
+        // List of all autonomous paths folders
+        List<File> paths = new ArrayList<File>() {
+            {
+                File pathsFile = null;
 
-            for (File f : directory.listFiles()) {
-              if (f.getPath().contains("paths")) {
-                pathsFile = f;
-              }
-            }
-
-            // get all contents of directory
-            try {
-              File[] contents = pathsFile.listFiles();
-
-              // go through files
-              for (File f : contents) {
-
-                // determine which are directories and
-                // only add those to paths.
-                // This is just a failsafe.
-                if (f.isDirectory()) {
-                  add(f);
+                for (File f : directory.listFiles()) {
+                    if (f.getPath().contains("paths")) {
+                        pathsFile = f;
+                    }
                 }
-              }
-            } catch (Exception e) {
+
+                // get all contents of directory
+                try {
+                    File[] contents = pathsFile.listFiles();
+
+                    // go through files
+                    for (File f : contents) {
+
+                        // only add directories to exclude miscellaneous files
+                        if (f.isDirectory()) {
+                            add(f);
+                        }
+                    }
+                } catch (Exception e) {}
             }
-          }
         };
 
-    // add each path
-    paths.forEach(
-        path -> {
-          // get the path
-          String pathname = path.getName();
+        // add each path
+        paths.forEach(
+                path -> {
+                    // get the path
+                    String pathname = path.getName();
 
-          // replace '_' with ' ' for readability purposes
-          pathChooser.addOption(pathname.replace('_', ' '), path.getPath());
-        });
+                    // replace '_' with ' ' for readability purposes
+                    pathChooser.addOption(pathname.replace('_', ' '), path.getPath());
+                });
 
-    SmartDashboard.putData("Autonomous Paths", pathChooser);
-  }
+        SmartDashboard.putData("Autonomous Paths", pathChooser);
+    }
 
-  public Command getCommand() {
-    SequentialCommandGroup command = new SequentialCommandGroup();
-    AutonomousFactory auto = new AutonomousFactory();
+    public Command getCommand() {
+        AutonomousFactory auto = new AutonomousFactory(container);
+        Command command = auto.createPath(pathChooser.getSelected()); // creates path command
 
-    /* TODO toggle necessary subsystems */
-    command.addCommands(
-        auto.createPath(container, pathChooser.getSelected())); // creates path command
-
-    return command;
-  }
+        return command;
+    }
 }
