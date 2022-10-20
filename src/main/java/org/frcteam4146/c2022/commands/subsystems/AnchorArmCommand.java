fd30e08553b8fb4dfd5a7af30b873353efc1adcb
Jake Weiss <jakedweiss8@gmail.com>
2022-10-20 15:52:25 -0700

    anchor arm command

---
+++ src/main/java/org/frcteam4146/c2022/commands/subsystems/AnchorArmCommand.java
@@ -0,0 +1,39 @@
+package org.frcteam4146.c2022.commands.subsystems;
+
+import org.frcteam4146.c2022.Constants.ClimbConstants;
+import org.frcteam4146.c2022.subsystems.Climb;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class AnchorArmCommand extends CommandBase {
+    
+    private final Climb climb;
+
+    public AnchorArmCommand(Climb climb) {
+
+        this.climb = climb;
+
+    }
+
+    @Override
+    public void execute() {
+
+        climb.setAnchorArmHeight(ClimbConstants.MAX_HEIGHT);
+        
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return climb.isExtended();
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+        climb.stopAnchorArm();
+
+    }
+
+}
